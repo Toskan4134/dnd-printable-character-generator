fbfabc0da0b7ac86f5ded55bfb5042fd67bc56b2
Toskan4134 <angama4134@gmail.com>
2025-02-12 21:19:58 +0100

    Remove Eslint

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
     basePath: '/dnd-printable-character-generator',
     output: 'export',
     reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
